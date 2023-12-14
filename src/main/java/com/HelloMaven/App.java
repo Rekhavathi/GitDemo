600bc8e6576365508d87ff77879f75caef2e7fc0
rekha <rekhavathi.g@gmail.com>
2023-12-14 13:59:40 +0530

    first commit

---
+++ src/main/java/com/HelloMaven/App.java
@@ -0,0 +1,16 @@
+package com.HelloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World1!" );
+        System.out.println( "Hello World2!" );
+        System.out.println( "Hello World3!" );
+    }
+}
